994bd6a3f0910965f39f345cdcc1ee8a86772505
Faleye Olumide Hakeem <faleyehakeem@yahoo.com>
2024-03-12 18:19:22 -0400

    fresh

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let myName = "Faleye Hakeem Olumide";
+document.getElementById("heading1").innerHTML = myName;
+alert(myName)
+console.log(myName);
+let age = "my age is" + "" + "" + "27yrs";
+document.getElementById("aged").innerHTML = age;
+console.log(age)
+const place = "Osun State";
+document.getElementById("placed").innerHTML = place;
+console.log(place)
+var $ = "I am a programmer and my mentor is Sunday omolewu";
+document.getElementById("skill").innerHTML = $;
+console.log($)
